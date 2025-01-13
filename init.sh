266038d218cbfd9a250930f2685226416028c4c6
kkb-1 <2609479523@qq.com>
2025-01-13 13:36:47 +0800

    second commit

---
+++ init.sh
@@ -0,0 +1,14 @@
+echo "init start..." \
+# init
+sudo mkdir /docker-env \
+# mysql
+sudo rm -rf /docker-env/mysql ; sudo mkdir -p /docker-env/mysql/data /docker-env/mysql/conf  && sudo chown -R 1001:1001 /docker-env/mysql \
+# postgeres
+# redis
+sudo rm -rf /docker-env/redis ; sudo mkdir -p /docker-env/redis/conf /docker-env/redis/data && sudo chown -R 1001:1001 /docker-env/redis \
+# etcd
+sudo rm -rf /docker-env/etcd ; sudo mkdir -p /docker-env/etcd/data /docker-env/etcd/conf && sudo chown -R 1001:1001 /docker-env/etcd \
+# kafka
+sudo rm -rf /docker-env/kafka ; sudo mkdir -p /docker-env/kafka/conf /docker-env/kafka/data && sudo chown -R 1001:1001 /kafka/redis \
+
+echo "init finish"
