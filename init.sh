237c029292efc0e6d95b469a3646f050c5daa6b8
kkbp <14321947+kkbp@user.noreply.gitee.com>
2025-01-13 23:27:38 +0800

    add jenkins

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 echo "init start..." \
 # init
 sudo mkdir /docker-env \
+# jenkins
+sudo rm -rf /docker-env/jenkins ; sudo mkdir -p /docker-env/jenkins/data && sudo chmod 777 /docker-env/jenkins \
 # mysql
 sudo rm -rf /docker-env/mysql ; sudo mkdir -p /docker-env/mysql/data /docker-env/mysql/conf  && sudo chown -R 1001:1001 /docker-env/mysql \
 # postgeres
@@ -9,6 +11,6 @@ sudo rm -rf /docker-env/redis ; sudo mkdir -p /docker-env/redis/conf /docker-env
 # etcd
 sudo rm -rf /docker-env/etcd ; sudo mkdir -p /docker-env/etcd/data /docker-env/etcd/conf && sudo chown -R 1001:1001 /docker-env/etcd \
 # kafka
-sudo rm -rf /docker-env/kafka ; sudo mkdir -p /docker-env/kafka/conf /docker-env/kafka/data && sudo chown -R 1001:1001 /kafka/ \
+sudo rm -rf /docker-env/kafka ; sudo mkdir -p /docker-env/kafka/conf /docker-env/kafka/data && sudo chown -R 1001:1001 /docker-env/kafka \
 
 echo "init finish"
